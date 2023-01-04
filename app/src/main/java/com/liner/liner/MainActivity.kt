a4b1660f30df311db9f8b82396b476f980948169
jibminJung <jibminjung@gmail.com>
2023-01-04 20:20:58 +0900

    initial setup

--- app/src/main/java/com/liner/liner/MainActivity.kt
+++ app/src/main/java/com/liner/liner/MainActivity.kt
@@ -2,10 +2,17 @@ package com.liner.liner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Text("Hello world")
+        }
     }
 }
